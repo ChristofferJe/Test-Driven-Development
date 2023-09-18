8880eb655023623f9168d3df018e3ddac6aacb3c
Bjarke Kjær <202106669@post.au.dk>
2023-09-18 14:56:40 +0200

    Snapshot: Ansvaret for mana-strategien er flyttet til eget interface og en klasse til at håndtere mana for hhv. alpha- og beta-version er implementeret.

---
+++ src/main/java/hotstone/framework/Version.java
@@ -0,0 +1,3 @@
+package hotstone.framework;
+
+public enum Version {ALPHA, BETA, GAMMMA, DELTA}
---
+++ src/main/java/hotstone/variants/ManaStrategy.java
@@ -0,0 +1,10 @@
+package hotstone.variants;
+
+import hotstone.framework.*;
+import hotstone.standard.*;
+
+
+public interface ManaStrategy {
+
+    void restoreMana(Hero hero, int turnNumber);
+}
