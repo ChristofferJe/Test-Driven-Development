844a28909bac99d870816dfdeb0f68768c618c17
Christoffer Jensen <202007962@post.au.dk>
2023-09-22 13:04:35 +0200

    Snapshot: Refactorering af BetaManaStrategy

--- src/main/java/hotstone/variants/BetaManaStrategy.java
+++ src/main/java/hotstone/variants/BetaManaStrategy.java
@@ -7,12 +7,8 @@ import hotstone.framework.*;
 public class BetaManaStrategy implements ManaStrategy {
     @Override
     public int calculateMana(int turnNumber) {
-        if(turnNumber % 2 == 0){
-            return Math.min(7, turnNumber / 2 );
-
-        } else{
-            return Math.min(7, turnNumber / 2 + 1);
-        }
+        if(turnNumber % 2 == 0){ return Math.min(7, turnNumber / 2 ); }
+        return Math.min(7, turnNumber / 2 + 1);
     }
 
 
